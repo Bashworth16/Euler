621ecd56de058be1bc49e84f595caaa34678643b
Ben Ashworth <bashworth16@yahoo.com>
2020-10-22 19:43:03 -0500

    005.py solved

--- 005.py
+++ 005.py
@@ -8,7 +8,7 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 result = 1
 START = 2
-END = 10
+END = 20
 num = 0
 THE_RANGE = range(START, END + 1)
 COMMON_MULTIPLES = {}
@@ -18,19 +18,17 @@ for x in THE_RANGE:
 
 print(result)
 
-# Write While loop to reduce result to the Least Common Multiple for each number in the range...
-for x in THE_RANGE:
-    q = result // x
-    print(f'{result} // {x} = {q}')
-    ok = True
-    for y in THE_RANGE:
-        if q % y != 0:
-            ok = False
-            break
-    if ok:
-        result = q
-        print('Reduced result to ', result)
 
-print(result)
+for x in THE_RANGE:  # By setting the x and y loops directly inside each other,
+    for y in THE_RANGE:  # we only change the denominator (x) if the quotient is no longer evenly divisible by each number in THE_RANGE
+        q = result // y   # q is the quotient being checked by each number in THE_RANGE.
+        ok = True         # This is my Flag check in order to update result and break out of the loop if the conditions are met.
+        for i in THE_RANGE:  # This loop is to Check to see if the quotient is divisible by each number in THE_RANGE
+            if q % i != 0:   # If not evenly divisible,
+                ok = False   # Switch
+                break        # break
+        if ok and q < result:  # If we reach this point, then quotient is evenly divisible by all number in THE_RANGE and is the 'lesser' common multiple,
+            result = q         # Update result with the "Lesser" common multiple.
+            print('Reduced result to ',result)
 
-
+print('LCM =',result)
